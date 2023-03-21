0abf44d27a2300d705ea9c2b025681b398df148f
tungddph28817 <tungddph28817@fpt.edu.vn>
2023-03-21 22:04:04 +0700

    tung_giao dien

--- app/src/main/java/com/nhom9_cp18013/appqlkh/MainActivity.java
+++ app/src/main/java/com/nhom9_cp18013/appqlkh/MainActivity.java
@@ -1,14 +1,34 @@
 package com.nhom9_cp18013.appqlkh;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawer;
+    Toolbar toolbar;
+    View mHeaderView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawer = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.tlbMain);
+        // set toolbar thay th cho actionbar
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu_24);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        FragmentManager manager = getSupportFragmentManager();
+        NavigationView nv = findViewById(R.id.nav_view);
+        // show
+        mHeaderView = nv.getHeaderView(0);
     }
 }
