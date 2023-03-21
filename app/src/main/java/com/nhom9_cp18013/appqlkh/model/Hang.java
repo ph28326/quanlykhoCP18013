ee79c5c68533c52552dd2bfbfade90a7a42f9835
Khánh Toàn <toantqkph28326@fpt.edu.vn>
2023-03-21 20:57:50 +0700

    ToanTQK PH28326: tạo model

---
+++ app/src/main/java/com/nhom9_cp18013/appqlkh/model/Hang.java
@@ -0,0 +1,36 @@
+package com.nhom9_cp18013.appqlkh.model;
+
+public class Hang {
+    private String maHang;
+    private String tenHang;
+    private byte[] hinhAnh;
+
+    public static final String TB_NAME = "Hang";
+    public static final String TB_COL_ID= "maHang";
+    public static final String TB_COL_NAME = "tenHang";
+    public static final String TB_COL_IMAGE = "hinhAnh";
+
+    public String getMaHang() {
+        return maHang;
+    }
+
+    public void setMaHang(String maHang) {
+        this.maHang = maHang;
+    }
+
+    public String getTenHang() {
+        return tenHang;
+    }
+
+    public void setTenHang(String tenHang) {
+        this.tenHang = tenHang;
+    }
+
+    public byte[] getHinhAnh() {
+        return hinhAnh;
+    }
+
+    public void setHinhAnh(byte[] hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+}
