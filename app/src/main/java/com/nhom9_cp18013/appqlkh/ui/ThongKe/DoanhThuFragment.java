7b8b94d0838feda392c4d16d5e5a75ed8e644072
tungddph28817 <tungddph28817@fpt.edu.vn>
2023-03-22 00:19:38 +0700

    tung_cập Nhập

---
+++ app/src/main/java/com/nhom9_cp18013/appqlkh/ui/ThongKe/DoanhThuFragment.java
@@ -0,0 +1,23 @@
+package com.nhom9_cp18013.appqlkh.ui.ThongKe;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nhom9_cp18013.appqlkh.R;
+
+
+public class DoanhThuFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_doanh_thu, container, false);
+    }
+}
---
+++ app/src/main/java/com/nhom9_cp18013/appqlkh/ui/ThongKe/topSanPhamFragment.java
@@ -0,0 +1,23 @@
+package com.nhom9_cp18013.appqlkh.ui.ThongKe;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nhom9_cp18013.appqlkh.R;
+
+
+public class topSanPhamFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top_san_pham, container, false);
+    }
+}
